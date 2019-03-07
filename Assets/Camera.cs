78f314d8bffbf98666f76dd4961a3cfb9401dbcd
Skipper_Jaxson <skipj515@hsd.k12.or.us>
2019-03-07 10:34:08 -0800

    added basic ball mechanics

---
+++ Assets/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    public GameObject target;
+    public float xOffset, yOffset, zOffset;
+ 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.transform.position + new Vector3(xOffset, yOffset, zOffset);
+       transform.LookAt(target.transform.position);
+    }
+}
---
+++ Assets/ballcontrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballcontrol : MonoBehaviour
+{
+    public float ballSpeed;
+
+    void Start()
+
+    {
+        
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+
+        Rigidbody body = GetComponent<Rigidbody>();
+        body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * ballSpeed * Time.deltaTime);
+
+
+    }
+}
+
